25f2f32037abc2a7ec30b96ae2fa98e45f6ff7f4
liuzhouhua <liuzhouhua_lzh@163.com>
2015-10-26 20:48:33 +0800

    15/10/26 01

---
+++ src/com/example/mobilesafe/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobilesafe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+}
